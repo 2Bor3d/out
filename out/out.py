3c4bbb468c974100a552ff84c07b8f18186e1356
2Bor3d <david.glaenzel@gmail.com>
2024-05-16 16:53:47 +0200

    added inout function

--- out/out.py
+++ out/out.py
@@ -1,3 +1,18 @@
-def out(text: any, color: int=38, *args):
-    print(f"\033[{color}m{text}\033[0m")
+def out(text, color: int=38, *args, output=True):
+    combine = f"\033[{color}m{text}"
+    for i in range(int(len(args)/2)):
+        combine += f"\033[{args[i*2+1]}m{args[i*2]}"
+    combine += "\033[0m"
     
+    if output:
+        print(combine)
+    return combine
+
+
+def inp(text, color: int=38, *args):
+    return input(out(text, color, *args, output=False))
+    
+
+def options(reach: int=100):
+    for i in range(range):
+        out(i, i)
