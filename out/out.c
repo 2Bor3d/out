e1be79d5a45e199518ac6e4e87a7f48a857af51a
2bor3d <david.glaenzel@gmail.com>
2024-12-14 22:18:23 +0100

    added missing files of previous commit

---
+++ out/out.c
@@ -0,0 +1,26 @@
+#include <Python.h>
+
+static PyObject* outFunc(PyObject* self, PyObject* args) {
+    printf("\x1B[31m");
+    printf("Hello World\n");
+    printf("\x1b[31m1\x1b[0m");
+    return Py_None;
+  }
+
+static PyMethodDef outMethodes[] = {
+    {"outFunc", outFunc, METH_NOARGS, "Python interface"},
+    {NULL, NULL, 0, NULL}
+};
+
+static struct PyModuleDef outModule = {
+    PyModuleDef_HEAD_INIT,
+    "outModule",
+    "Python interface for the fputs C library function",
+    -1,
+    outMethodes
+};
+
+PyMODINIT_FUNC PyInit_out(void) {
+    return PyModule_Create(&outModule);
+}
+
---
+++ test.py
@@ -0,0 +1,4 @@
+import out
+
+out.main();
+print("done")
